962de9a2b0e0eb95bbbf989dd69e85b07e3768f0
chansamangrathana <chansamangrathana1607@gmail.com>
2024-03-31 09:22:56 -0700

    logo

--- components/navbar/NavbarComponent.tsx
+++ components/navbar/NavbarComponent.tsx
@@ -43,12 +43,12 @@ export default function NavbarComponent() {
 		<Navbar fluid rounded className="w-full">
 			<NavbarBrand as={Link} href="https://flowbite-react.com">
 				<img
-					src="/next.svg"
+					src="https://istad.co/resources/img/CSTAD_120.png"
 					className="mr-3 h-6 sm:h-9"
 					alt="Flowbite React Logo"
 				/>
 				<span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-					Flowbite React
+					CSTAD
 				</span>
 			</NavbarBrand>
 			<NavbarToggle />
