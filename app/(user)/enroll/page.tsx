6604734a29ac1367eda93fa11d5c7e83f7f88b0c
chansamangrathana <chansamangrathana1607@gmail.com>
2024-04-02 18:37:07 -0700

    end neww

--- app/(user)/enroll/page.tsx
+++ app/(user)/enroll/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function page() {
 	return (
 		<>
-			<div className="h-screen grid place-content-center text-6xl">Enroll</div>
+			<div className="h-screen grid place-content-center text-6xl bg-red-600">Enroll</div>
 		</>
 	);
 }
